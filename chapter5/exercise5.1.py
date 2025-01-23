bfbc714efd21bde7ecdaa79d4172e38f39068457
zhang.weifeng <zhang.weifeng@tslsmart.com>
2025-01-23 14:20:08 +0800

    增加第五章练习题

---
+++ chapter5/exercise5.1.py
@@ -0,0 +1,19 @@
+def displayInventory(inventory):
+    print('Inventory:')
+    sum = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + str(k))
+        sum += v
+
+    print('Total number of items: ' + str(sum))
+
+
+inventory = {
+    'rope': 1,
+    'torch': 6,
+    'gold coin': 42,
+    'dagger': 1,
+    'arrow': 12
+}
+
+displayInventory(inventory)
