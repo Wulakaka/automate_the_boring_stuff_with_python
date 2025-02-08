e1f10da189a432a117d4d364fe7ce7217e79b422
zhang.weifeng <zhang.weifeng@tslsmart.com>
2025-02-08 15:09:22 +0800

    feat(chapter9): 调整练习：消除缺失的编号中获取文件名列表的方法

--- chapter9/exercises/removeMissingNumbers.py
+++ chapter9/exercises/removeMissingNumbers.py
@@ -2,23 +2,24 @@
 # 消除缺失的编号
 # 9.8.3
 
-import os,re,shutil
+import os, re, shutil
+
 
 def removeMissingNumbers(dirname, prefix):
     regex = re.compile('^' + prefix + r'(\d{3}).txt$')
-    for folderName, subfolders, filenames in os.walk(dirname):
-        numbers = []
-        for filename in filenames:
-            result = regex.search(filename)
-            if result is not None:
-                numbers.append(int(result.group(1)))
+    filenames = os.listdir(dirname)
+    numbers = []
+    for filename in filenames:
+        result = regex.search(filename)
+        if result is not None:
+            numbers.append(int(result.group(1)))
 
-        numbers.sort()
-        for i in range(len(numbers)):
-            if numbers[i] != i + 1:
-                newName = prefix + str(i + 1).zfill(3) + '.txt'
-                oldName = prefix + str(numbers[i]).zfill(3) + '.txt'
-                shutil.move(os.path.join(folderName, oldName), os.path.join(folderName, newName))
+    numbers.sort()
+    for i in range(len(numbers)):
+        if numbers[i] != i + 1:
+            newName = prefix + str(i + 1).zfill(3) + '.txt'
+            oldName = prefix + str(numbers[i]).zfill(3) + '.txt'
+            shutil.move(os.path.join(dirname, oldName), os.path.join(dirname, newName))
 
 
-removeMissingNumbers('./test','test')
+removeMissingNumbers('./test', 'test')
