a2d2fe40e5944b64eb3dda347550445748f96de4
张伟峰 <zhang.weifeng@tslsmart.com>
2025-02-14 10:27:10 +0800

    format(chapter12): 格式化

--- chapter12/updateProduce.py
+++ chapter12/updateProduce.py
@@ -14,7 +14,7 @@ PRICE_UPDATES = {
 }
 
 # Loop through the rows and update the prices.
-for rowNum in range(2, sheet.max_row): # skip the first row
+for rowNum in range(2, sheet.max_row):  # skip the first row
     produceName = sheet.cell(row=rowNum, column=1).value
     if produceName in PRICE_UPDATES:
         sheet.cell(row=rowNum, column=2).value = PRICE_UPDATES[produceName]
